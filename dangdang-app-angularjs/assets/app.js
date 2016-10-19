62f5c5e037473d9b6310a4440abc4e96b92c35b1
mac <mac@DDDDDDDDDDDD.local>
2016-10-19 21:26:43 +0800

    ---

--- dangdang-app-angularjs/assets/app.js
+++ dangdang-app-angularjs/assets/app.js
@@ -17,12 +17,12 @@ app.config(['$routeProvider',function($routeProvider){
     templateUrl:'./assets/tpl/about_me.html',
     controller:''
   })
-  .when('/book/book_list',{
+  .when('/book_list',{
     templateUrl:'./assets/tpl/book_list.html',
     controller:'bookListController'
+  })
+  .otherwise({
+    redirectTo:'/'
   });
-  // .otherwise({
-  //   redirectTo:'/'
-  // });
 
 }])
