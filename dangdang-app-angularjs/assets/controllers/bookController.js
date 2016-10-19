8f362a9be569e0bdf3f888036e3f4e409c2f548b
mac <mac@DDDDDDDDDDDD.local>
2016-10-19 17:18:32 +0800

    提交

--- dangdang-app-angularjs/assets/controllers/bookController.js
+++ dangdang-app-angularjs/assets/controllers/bookController.js
@@ -1,9 +1,9 @@
 app.controller('bookController',['$scope',function($scope){
   $scope.bookTypes=[
-    {id:'ertong',name:'儿童',img:'../images/24026269-1_l_11.jpg',description:''},
-    {id:'lishi',name:'历史',img:'../images/24036444-1_l_10.jpg',description:''},
-    {id:'dongman',name:'动漫',img:'../images/24017630-1_l_9.jpg',description:''},
-    {id:'qingchunwenxue',name:'青春文学',img:'../images/24024364-1_l_12.jpg',description:''},
+    {id:'ertong',name:'儿童',img:'./assets/images/24026269-1_l_11.jpg',description:''},
+    {id:'lishi',name:'历史',img:'./assets/images/24036444-1_l_10.jpg',description:''},
+    {id:'dongman',name:'动漫',img:'./assets/images/24017630-1_l_9.jpg',description:''},
+    {id:'qingchunwenxue',name:'青春文学',img:'./assets/images/24024364-1_l_12.jpg',description:''},
   ];
 
 }]);
