c54b1d3baa9da495207510662a987f67c255e591
mac <mac@DDDDDDDDDDDD.local>
2016-10-19 15:20:38 +0800

    初始化一个完整框架

---
+++ dangdang-app-angularjs/assets/api/service.js
@@ -0,0 +1,23 @@
+// 创建一个公用数据响应服务
+app.factory('commonService',['$http',function($http){
+  var service={};
+  /**
+   * 根据分类信息取数据
+   * @param  {[type]} type     [description]
+   * @param  {[type]} callBack [description]
+   * @return {[type]}          [description]
+   */
+  service.getData =function(type,callBack){
+    $http({
+      url:'../../data/book_'+type+'.json',
+      method:'get'
+    })
+    .then(function(res){
+      console.log('获取数据成功');
+      callBack(res);
+    },function(err){
+      console.log(err);
+    })
+  }
+  return service;
+}]);
