9ca543d8b2436ec05cfa0bf3561d96b3016a2f75
mac <mac@DDDDDDDDDDDD.local>
2016-10-27 21:23:42 +0800

    11

--- 前端项目二当当网于博客/asserts/controllers/bookController.js
+++ 前端项目二当当网于博客/asserts/controllers/bookController.js
@@ -1,5 +1,5 @@
 app.controller('bookController',['$scope',function($scope){
-  $.scope.bookType=[
+  $scope.bookType=[
     {id:'ertong',name:'儿童',img:'./asserts/img/24026269-1_l_11.jpg',description:'儿童书籍大全'},
     {id:'lishi',name:'历史',img:'./asserts/img/24036444-1_l_10.jpg',description:'历史书籍大全'},
     {id:'dongman',name:'动漫',img:'./asserts/img/24017630-1_l_9.jpg',description:'动漫书籍大全'},
