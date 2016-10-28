f5b34e27ab129e4d746dc7eaf98b3df31cc2ffb8
mac <mac@DDDDDDDDDDDD.local>
2016-10-28 21:20:17 +0800

    提交

--- dangdang-app-angularjs/assets/controllers/bookController.js
+++
@@ -1,9 +0,0 @@
-app.controller('bookController',['$scope',function($scope){
-  $scope.bookTypes=[
-    {id:'ertong',name:'儿童',img:'./assets/images/24026269-1_l_11.jpg',description:'儿童书籍大全'},
-    {id:'lishi',name:'历史',img:'./assets/images/24036444-1_l_10.jpg',description:'历史书籍大全'},
-    {id:'dongman',name:'动漫',img:'./assets/images/24017630-1_l_9.jpg',description:'动漫书籍大全'},
-    {id:'qingchunwenxue',name:'青春文学',img:'./assets/images/24024364-1_l_12.jpg',description:'青春文学书籍大全'},
-  ];
-
-}]);
